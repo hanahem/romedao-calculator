17bd45053ba6dc64c0e11a88629fbbfc4ec7ce34
hanahem <wawa.h8@gmail.com>
2021-12-01 12:11:20 +0100

    :fire: Removing unused vars

--- utils/utils.ts
+++ utils/utils.ts
@@ -1,20 +1,9 @@
-import axios from "axios";
 import { ethers } from "ethers";
 import fromExponential from "from-exponential";
 import { romeFraxPair } from "../abis/romeFraxPair.abi";
 import { addresses } from "./constants";
 
 const cache: { [key: string]: number } = {};
-
-export const loadTokenPrices = async () => {
-  const url =
-    "https://api.coingecko.com/api/v3/simple/price?ids=olympus,chainlink,curve-dao-token,frax-share&vs_currencies=usd";
-  const { data } = await axios.get(url);
-
-  cache["OHM"] = data["olympus"].usd;
-  cache["CRV"] = data["curve-dao-token"].usd;
-  cache["FXS"] = data["frax-share"].usd;
-};
 
 export const getTokenPrice = (symbol: string): number => {
   return Number(cache[symbol]);
