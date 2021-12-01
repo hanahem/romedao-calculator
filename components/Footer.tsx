89dd4a8e9b8827667d321ab7571fabcec605f9b6
hanahem <wawa.h8@gmail.com>
2021-12-01 12:10:36 +0100

    :rotating_light: Linter warning fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,10 +8,10 @@ const Footer = () => {
         Not affiliated to <a className="underline" href="https://romedao.finance">RomeDAO</a>
       </span>
       <div className="flex items-center gap-4 text-gray-500">
-        <a href="https://twitter.com/0Xethercake" target="_blank">
+        <a href="https://twitter.com/0Xethercake" target="_blank" rel="noreferrer">
           <FaTwitter size={15} />
         </a>
-        <a href="https://github.com/hanahem" target="_blank">
+        <a href="https://github.com/hanahem" target="_blank" rel="noreferrer">
           <FaGithub size={15} />
         </a>
       </div>
